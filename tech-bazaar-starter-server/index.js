b9f4638a5f7bf1775d50ce2c48bfc791e43b9a48
umayer10-AI <mdumayer10@gmail.com>
2026-06-15 19:28:14 +0600

    first commit

---
+++ tech-bazaar-starter-server/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const dontenv = require("dotenv");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+dontenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: [process.env.CLIENT_URL],
+  }),
+);
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("tech-bazaar");
+
+    
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+  } 
+  finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server is running fine!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
